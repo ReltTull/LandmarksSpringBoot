b141e8b3fc03986edf2682114f60b29384708bd2
ReltTull <roman3202@gmail.com>
2024-11-13 22:26:40 +0300

    Repo fixed implementaion -> extends

--- src/main/java/com/example/Repositories/LandmarksRepository.java
+++ src/main/java/com/example/Repositories/LandmarksRepository.java
@@ -3,5 +3,5 @@ package com.example.Repositories;
 import com.example.Entities.Lanmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LandmarksRepository implements JpaRepository<Lanmark, Long> {
+public interface LandmarksRepository extends JpaRepository<Lanmark, Long> {
 }
